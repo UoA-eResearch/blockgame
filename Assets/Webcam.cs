0c89a36e52bdeb66973189744ffbcfbd5f5c968a
Nick Young <nick.young@auckland.ac.nz>
2018-09-03 11:34:01 +1200

    display user webcam

---
+++ Assets/Webcam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Webcam : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		WebCamTexture webcamTexture = new WebCamTexture();
+		GetComponent<RawImage>().texture = webcamTexture;
+		webcamTexture.Play();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
